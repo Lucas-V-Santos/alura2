e6a4a45ee7ff6d044044c043d19bc4e9e22c4860
Lucas-V-Santos <lucasvieiraconsultoria@gmail.com>
2022-02-23 12:12:05 -0300

    Python 3: Módulo 5, Aula 04 (Alura)

--- conta.py
+++ conta.py
@@ -18,3 +18,27 @@ class Conta:
     def transferir(self, valor, recebedor):
         self.saca(valor)
         recebedor.depositar(valor)
+
+    def get_conta(self):
+        return self.__numero
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_conta(self, numero):
+        self.__numero = numero
+
+    def set_titula(self, titular):
+        self.__titular = titular
+
+    def set_saldo(self, saldo):
+        self.__saldo = saldo
+
+    def set_limite(self, limite):
+        self.__limite = limite
